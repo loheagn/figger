b108f2145355319ce306364e018771b22e4d99fa
loheagn <loheagn@icloud.com>
2023-05-11 14:56:21 +0800

    Use epoll instead of select
    Signed-off-by: loheagn <loheagn@icloud.com>

--- proc_test/main.go
+++ proc_test/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"syscall"
+	"time"
 	"unsafe"
 )
 
@@ -42,6 +43,39 @@ func (s *fdset) IsSet(fd uintptr) bool {
 	return s.Bits[n]&(1<<m) != 0
 }
 
+func readAndTrigger(filename string) {
+	readProcFile(filename)
+
+	err := exec.Command("bash", "-c", "systemctl start nginx").Run()
+	if err != nil {
+		panic(err)
+	}
+	err = exec.Command("bash", "-c", "ipvsadm -a -t 192.168.174.134:6699 -r 192.168.174.134:80 -m -w 1").Run()
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile(filename, []byte("hello world"), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func cleanup() {
+	err := exec.Command("bash", "-c", "systemctl stop nginx").Run()
+	if err != nil {
+		panic(err)
+	}
+	err = exec.Command("bash", "-c", "ipvsadm -D -t 192.168.174.134:6699").Run()
+	if err != nil {
+		panic(err)
+	}
+	err = exec.Command("bash", "-c", "ipvsadm -A -t 192.168.174.134:6699 -s rr").Run()
+	if err != nil {
+		panic(err)
+	}
+}
+
 // syscall.Select file
 func selectFile(filename string) {
 	var fd fdset
@@ -58,23 +92,49 @@ func selectFile(filename string) {
 
 	log.Printf("isset %v\n", fd.IsSet(file.Fd()))
 
-	readProcFile(filename)
+	readAndTrigger(filename)
+}
 
-	err = exec.Command("bash", "-c", "systemctl start nginx").Run()
+func epoll_test(filename string) {
+	epfd, err := syscall.EpollCreate1(0)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	err = exec.Command("bash", "-c", "ipvsadm -a -t 192.168.174.134:6699 -r 192.168.174.134:80 -m -w 1").Run()
+	defer syscall.Close(epfd)
+
+	file, err := os.Open(filename)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	err = os.WriteFile(filename, []byte("hello world"), 0644)
+	var event syscall.EpollEvent
+	event.Events = syscall.EPOLLIN
+	event.Fd = int32(file.Fd())
+
+	err = syscall.EpollCtl(epfd, syscall.EPOLL_CTL_ADD, int(file.Fd()), &event)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+
+	events := make([]syscall.EpollEvent, 1)
+	n, err := syscall.EpollWait(epfd, events, -1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("ready %d\n", n)
+
+	for _, event := range events {
+		if event.Fd == int32(file.Fd()) {
+			readAndTrigger(filename)
+		}
+	}
+
 }
 
 func main() {
-	selectFile(os.Args[1])
+	// selectFile(os.Args[1])
+	epoll_test(os.Args[1])
+
+	time.Sleep(10 * time.Second)
+	cleanup()
 }
